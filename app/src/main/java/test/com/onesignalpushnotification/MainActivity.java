589d413df77e72cacb4aabae67d5ce4e1ed2d1f2
vickkyr9@gmail.com <vickkyr9@gmail.com>
2018-09-13 09:50:32 +0500

    Version 1.0

---
+++ app/src/main/java/test/com/onesignalpushnotification/MainActivity.java
@@ -0,0 +1,21 @@
+package test.com.onesignalpushnotification;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.onesignal.OneSignal;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // OneSignal Initialization
+        OneSignal.startInit(this)
+                .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
+                .unsubscribeWhenNotificationsAreDisabled(true)
+                .init();
+    }
+}
